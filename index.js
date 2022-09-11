a940057c82b5a8bd34d403c266c6e19c1fc19b7f
Sathwik Pothunoori <sathwikpothunoori@gmail.com>
2022-09-11 12:13:45 +0530

    after accesing student database

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express =require('express')
 const mongoClient =require('mongodb').MongoClient
 const assert =require('assert')
 
+// address of the database
 const url = 'mongodb://localhost:27017/'
 
-const dbname ='conFusion'
+const dbname ='Student'
 
+// connecting to database server
 mongoClient.connect(url , (err , client)=>{
 
     assert.equal(err,null)
-    
 
+    console.log('connected properly to the server')
+
+    const db = client.db(dbname)  // accesing  a database
+
+    const collection = db.collection('it') //it provides various different functions to use on a database
+
+    collection.insertOne({"name":"sai" ,"roll":"107"} , (err , result)=>{
+        assert.equal(err ,null)
+
+        console.log(result.ops)
+
+        collection.find({}).toArray((err , docs)=>{
+            assert.equal(err ,null)
+            
+            console.log('Found:\n ')
+            console.log(docs)
+
+            //cleaning the db
+            db.dropCollection('it' , (err , result)=>{
+                assert.equal(err , null)
+
+                client.close()
+            })
+
+
+        })
+    })
 })
